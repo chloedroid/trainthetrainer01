90166514fae009f38c91ef9872ae2268ec036ec0
gelderch <chloe_van_gelder@hotmail.com>
2021-08-17 11:21:57 +0200

    result

--- interface.py
+++ interface.py
@@ -1,18 +1,28 @@
 from tkinter import *
-from tkinter import messagebox
-
 
 app = Tk()
+app.title('Train the Trainer 1')
+app.configure(bg="white")
+app.geometry('400x400')
 
-def ditinfo():
-    info = Label(app, text="DIT = Data Interventie Team", font=('Comic Sans', 18, 'bold'))
-    info.grid(row=2, column=1)
+def info():
+    infoo = Label(app, text='Hello ' + input.get(), font=('Helvetica', 18, 'bold'))
+    infoo.grid(row=3, column=1)
 
-DIT = Button(app, text="Your name?", command=ditinfo)
+DIT = Button(app, text="Your name?", fg="red", bg="blue", borderwidth=30, font=('Comic Sans', 18, 'bold'), command=info)
 DIT.grid(row=1, column=1)
 
-app.title"Probeersel"
-app.configure(bg="red")
+input = Entry(app)
+input.grid(row=6, column=3)
+
+check = Checkbutton(app, text="data")
+check.grid(row=8, column=3)
+
+namenlijst = StringVar(app)
+namenlijst.set("Kies een naam")
+menu = OptionMenu(app, namenlijst, "Sofie", "Saar", "Flore", "Chloë", "Susanna", "Tine")
+menu.grid(row=10, column=1)
+
 
 app.mainloop()
 
